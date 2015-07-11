cfc6b03c4f2904c8b05ed741613b27c83a79ddf1
Himani Plaha <hplaha@Himanis-MacBook-Pro.local>
2015-07-11 00:41:03 -0700

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Himani Plaha on 7/11/15.
+//  Copyright (c) 2015 Jehovah Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
