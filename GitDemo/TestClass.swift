7faed12d7e30648e5b1c0549722bcd602bb6d7e6
Himani Plaha <hplaha@Himanis-MacBook-Pro.local>
2015-07-11 01:16:29 -0700

    TestClass was added to the project.

---
+++ GitDemo/TestClass.swift
@@ -0,0 +1,13 @@
+//
+//  TestClass.swift
+//  GitDemo
+//
+//  Created by Himani Plaha on 7/11/15.
+//  Copyright (c) 2015 Jehovah Media. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+   
+}
